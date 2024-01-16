7829d07cabfc6fba2de9e1a99d05f966a1465af2
TomF46 <t-d-f@live.co.uk>
2024-01-16 15:12:04 +0000

    Add image description for project images.

--- src/views/ProjectDetail.vue
+++ src/views/ProjectDetail.vue
@@ -1,149 +1,233 @@
 <template>
-  <div id="project-page" class="projects page project-detail">
-    <div v-if="project">
-    <div class="row project-row no-border-b">
-      <div class="col-xs-12 mb-16">
-        <div class="row">
-          <div class="card">
-            <div class="col-xs-12 center-xs start-sm">
-              <h1 class="page-title">{{ project.name }}</h1>
+    <div id="project-page" class="projects page project-detail">
+        <div v-if="project">
+            <div class="row project-row no-border-b">
+                <div class="col-xs-12 mb-16">
+                    <div class="row">
+                        <div class="card">
+                            <div class="col-xs-12 center-xs start-sm">
+                                <h1 class="page-title">
+                                    {{ project.name }}
+                                </h1>
+                            </div>
+                            <div class="col-xs-12">
+                                <p>
+                                    <span class="bold">Description:</span>
+                                    {{ project.description }}
+                                </p>
+                            </div>
+                            <div
+                                v-if="project.technology.length > 0"
+                                class="col-xs-12"
+                            >
+                                <p class="bold">Technology used:</p>
+                                <ul class="tech-list">
+                                    <li
+                                        v-for="(
+                                            technology, i
+                                        ) in project.technology"
+                                        :key="i"
+                                    >
+                                        {{ technology }}
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="col-xs-12 button-row">
+                                <button
+                                    class="button"
+                                    v-if="project.link != null"
+                                    @click="externalNavigate(project.link)"
+                                >
+                                    Project website
+                                </button>
+                                <button
+                                    class="button"
+                                    v-if="project.githubLink != null"
+                                    @click="
+                                        externalNavigate(project.githubLink)
+                                    "
+                                >
+                                    Github
+                                </button>
+                                <button
+                                    class="button"
+                                    v-if="project.readMeLink != null"
+                                    @click="
+                                        externalNavigate(project.readMeLink)
+                                    "
+                                >
+                                    Detailed ReadMe
+                                </button>
+                            </div>
+                            <div class="col-xs-12">
+                                <div
+                                    class="row center-xs start-sm image-preview-row"
+                                >
+                                    <img
+                                        class="image"
+                                        v-for="(image, i) in project.images"
+                                        :key="i"
+                                        :src="image.src"
+                                        @click="index = i"
+                                        alt="project image"
+                                    />
+                                    <gallery
+                                        :images="project.images"
+                                        :index="index"
+                                        @close="index = null"
+                                        :options="options"
+                                    ></gallery>
+                                </div>
+                            </div>
+                            <div
+                                v-if="project.videos.length > 0"
+                                class="col-xs-12"
+                            >
+                                <h2 class="title">Videos</h2>
+                                <div class="row">
+                                    <div
+                                        v-for="(video, j) in project.videos"
+                                        :key="j"
+                                        class="col-xs-12 col-md-6 col-lg-4 video-container"
+                                    >
+                                        <p class="video-title">
+                                            {{ video.title }}
+                                        </p>
+                                        <iframe
+                                            class="video"
+                                            :src="video.src"
+                                            :title="video.title"
+                                            frameborder="0"
+                                            allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                            allowfullscreen
+                                        ></iframe>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="col-xs-12">
-              <p><span class="bold">Description:</span> {{ project.description }}</p>
-            </div>
-            <div v-if="project.technology.length > 0" class="col-xs-12">
-              <p class="bold">Technology used:</p>
-              <ul class="tech-list">
-                <li v-for="(technology, i) in project.technology" :key="i">{{technology}}</li>
-              </ul>
-            </div>
-            <div class="col-xs-12 button-row">
-            <button class="button" v-if="project.link != null" @click="externalNavigate(project.link)"
-              >Project website
-            </button>
-            <button class="button" v-if="project.githubLink != null" @click="externalNavigate(project.githubLink)">
-              Github
-            </button>
-            <button class="button" v-if="project.readMeLink != null" @click="externalNavigate(project.readMeLink)">
-              Detailed ReadMe
-            </button>
-          </div>
-          <div class="col-xs-12">
-            <div class="row center-xs start-sm image-preview-row">
-              <img class="image" v-for="(image, i) in project.images"  :key="i" :src="image" @click="index = i" alt="project image" />
-              <gallery :images="project.images" :index="index" @close="index = null"></gallery>
-            </div>
-          </div>
-          <div v-if="project.videos.length > 0" class="col-xs-12">
-            <h2 class="title">Videos</h2>
-            <div class="row">
-              <div v-for="(video, j) in project.videos" :key="j" class="col-xs-12 col-md-6 col-lg-4 video-container">
-                <p class="video-title">{{video.title}}</p>
-                <iframe class="video" :src="video.src" :title="video.title" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-              </div>
-            </div>
-          </div>
-          </div>
         </div>
-      </div>
+        <div v-else class="project-not-found-page">
+            <p class="text-center">
+                No project is found with the chosen identifier.
+            </p>
+            <button @click="navigate('/')">Homepage</button>
+        </div>
     </div>
-    </div>
-    <div v-else class="project-not-found-page">
-        <p class="text-center">No project is found with the chosen identifier.</p>
-        <button @click="navigate('/')">Homepage</button>
-    </div>
-  </div>
 </template>
 
 <script>
-import projectsData from '../projects.json';
-import VueGallery from 'vue-gallery';
+import projectsData from "../projects.json";
+import VueGallery from "vue-gallery";
 
 export default {
-  name: "Projects-Detail",
-  props: ['slug'],
-  components: {'gallery' :  VueGallery },
-  data: () => {
-    return {
-      projects: projectsData,
-      index: null
-    };
-  },
-  computed: {
-      project(){
-          return this.projects.find(project => project.slug == this.slug);
-      }
-  },
-  methods:{
-    navigate(link){
-      this.$router.push(link)
+    name: "Projects-Detail",
+    props: ["slug"],
+    components: { gallery: VueGallery },
+    data: () => {
+        return {
+            projects: projectsData,
+            index: null,
+            options: {
+                urlProperty: "src",
+            },
+        };
     },
-    externalNavigate(link){
-      window.location.href = link;
-    }
-  }
+    computed: {
+        project() {
+            return this.projects.find((project) => project.slug == this.slug);
+        },
+    },
+    methods: {
+        navigate(link) {
+            this.$router.push(link);
+        },
+        externalNavigate(link) {
+            window.location.href = link;
+        },
+    },
 };
 </script>
 
 <style lang="scss">
-.project-not-found-page{
+.project-not-found-page {
     padding-top: 10vh;
 }
 
-.button-row{
+.button-row {
     margin-bottom: 4vh;
-    button{
+    button {
         margin-right: 10px;
         padding: 6px;
     }
 }
 
-.project-detail{
-  .card{
-    margin: 40px;
-  }
+.project-detail {
+    .card {
+        margin: 40px;
+    }
 }
 
 .image {
-  width: 140px;
-  height: 140px;
-  background-size: cover;
-  cursor: pointer;
-  margin: 5px;
-  border-radius: 3px;
-  border: 1px solid #31708e;
-  object-fit: contain;
+    width: 140px;
+    height: 140px;
+    background-size: cover;
+    cursor: pointer;
+    margin: 5px;
+    border-radius: 3px;
+    border: 1px solid #31708e;
+    object-fit: contain;
 }
 .image:hover {
-  border: 1px solid #ADF1D2;
+    border: 1px solid #adf1d2;
 }
 
-.video-container{
-  .video-title{
-    color: #ADF1D2!important;
-  }
-  .video{
-    aspect-ratio: 16 / 9;
+.video-container {
+    .video-title {
+        color: #adf1d2 !important;
+    }
+    .video {
+        aspect-ratio: 16 / 9;
+        width: 100%;
+    }
+    padding: 8px;
+}
+
+.blueimp-gallery-controls > .title {
+    position: absolute;
+    top: auto;
+    left: auto;
+    right: auto;
+    bottom: 0;
     width: 100%;
-  }
-  padding: 8px;
+    background-color: #31708e;
+    text-align: center;
+    opacity: 100;
+    font-family: "Roboto", Avenir, Helvetica, Arial, sans-serif;
+    font-size: 28px;
+    padding: 10px;
 }
-
-
+.blueimp-gallery-controls > .close {
+    position: absolute;
+    top: 15px;
+    left: auto;
+    right: 15px;
+    bottom: auto;
+}
 
 @media (max-width: 47em) {
-    .button-row{
-        button{
+    .button-row {
+        button {
             margin: 0 auto;
             margin-top: 10px;
         }
     }
-    .project-detail{
-      .card{
-        margin: 0px;
-      }
+    .project-detail {
+        .card {
+            margin: 0px;
+        }
+    }
 }
-}
-
-
 </style>
--- src/views/ProjectsPage.vue
+++ src/views/ProjectsPage.vue
@@ -1,120 +1,140 @@
 <template>
-  <div id="projects-page" class="projects page">
-    <div class="row center-xs">
-      <div class="col-xs-12">
-        <h1 class="page-title">Example Projects</h1>
-      </div>
-    </div>
-    <div class="row">
-      <div v-for="(project, i) in projects" :key="i" class="col-xs-12 col-sm-6 col-md-4 project-card">
-        <div class="row">
-          <div class="col-xs-12" @click="navigate(`/projects/${project.slug}`)">
-            <crossfade-image  :topImage="project.images[0]" :bottomImage="project.images[1]" :title="project.name" />
-          </div>
-          <div class="col-xs-12">
-            <h3 class="title pointer" @click="navigate(`/projects/${project.slug}`)">{{ project.name }}</h3>
-          </div>
-          <div class="col-xs-12">
-            <p>{{ project.description }}</p>
-          </div>
-          <div v-if="project.link != null" class="col-xs-12">
-            <a class="project-external-link" :href="project.link">Project website</a>
-          </div>
-          <div v-if="project.githubLink != null" class="col-xs-12">
-            <a class="project-external-link" v-if="project.githubLink != null" :href="project.githubLink">Github page</a>
-          </div>
+    <div id="projects-page" class="projects page">
+        <div class="row center-xs">
+            <div class="col-xs-12">
+                <h1 class="page-title">Example Projects</h1>
+            </div>
         </div>
-      </div>
+        <div class="row">
+            <div
+                v-for="(project, i) in projects"
+                :key="i"
+                class="col-xs-12 col-sm-6 col-md-4 project-card"
+            >
+                <div class="row">
+                    <div
+                        class="col-xs-12"
+                        @click="navigate(`/projects/${project.slug}`)"
+                    >
+                        <crossfade-image
+                            :topImage="project.images[0].src"
+                            :bottomImage="project.images[1].src"
+                            :title="project.name"
+                        />
+                    </div>
+                    <div class="col-xs-12">
+                        <h3
+                            class="title pointer"
+                            @click="navigate(`/projects/${project.slug}`)"
+                        >
+                            {{ project.name }}
+                        </h3>
+                    </div>
+                    <div class="col-xs-12">
+                        <p>{{ project.description }}</p>
+                    </div>
+                    <div v-if="project.link != null" class="col-xs-12">
+                        <a class="project-external-link" :href="project.link"
+                            >Project website</a
+                        >
+                    </div>
+                    <div v-if="project.githubLink != null" class="col-xs-12">
+                        <a
+                            class="project-external-link"
+                            v-if="project.githubLink != null"
+                            :href="project.githubLink"
+                            >Github page</a
+                        >
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-import projectsData from '../projects.json';
-import CrossfadeImage from '../components/CrossfadeImage.vue';
+import projectsData from "../projects.json";
+import CrossfadeImage from "../components/CrossfadeImage.vue";
 
 export default {
-  name: "ProjectsPage",
-  components:{
-    'crossfade-image' : CrossfadeImage,
-  },
-  data: () => {
-    return {
-      projects: projectsData
-    };
-  },
-  methods:{
-    navigate(link){
-      this.$router.push(link)
+    name: "ProjectsPage",
+    components: {
+        "crossfade-image": CrossfadeImage,
     },
-  }
+    data: () => {
+        return {
+            projects: projectsData,
+        };
+    },
+    methods: {
+        navigate(link) {
+            this.$router.push(link);
+        },
+    },
 };
 </script>
 
 <style lang="scss">
 .projects {
-  text-align: left;
-  .project-card {
-    padding: 20px 40px;
-    
-    .project-info {
-      margin: 40px 0;
-    }
-    .project-external-link{
-      color: #ADF1D2;
-    }
-    .project-external-link:hover{
-      color: #31708e;
-    }
-  }
-  .title {
-    font-size: 1.4em;
-    color: #31708e;
-    margin-bottom: 8px;
-  }
-  button{
-    display: inline-block;
-    padding: 8px;
-    font-size: 1.1em;
-  }
-  .project-image {
-    width: 100%;
-    margin: 0 40px;
-    margin-left: auto;
-    margin-right: auto;
-    display: block;
-  }
-  a {
-    text-decoration: none;
-  }
-  a:hover {
-    color: #31708e;
-  }
-  .link{
-    color: #31708e;
-    font-size: 1.25em;
-    cursor: pointer;
-  }
-  .link:hover{
-    color: #31598e;
-  }
-
-  @media (max-width: 47em) {
+    text-align: left;
     .project-card {
-      padding: 20px;
+        padding: 20px 40px;
+
+        .project-info {
+            margin: 40px 0;
+        }
+        .project-external-link {
+            color: #adf1d2;
+        }
+        .project-external-link:hover {
+            color: #31708e;
+        }
+    }
+    .title {
+        font-size: 1.4em;
+        color: #31708e;
+        margin-bottom: 8px;
+    }
+    button {
+        display: inline-block;
+        padding: 8px;
+        font-size: 1.1em;
     }
     .project-image {
-      max-width: 100%;
+        width: 100%;
+        margin: 0 40px;
+        margin-left: auto;
+        margin-right: auto;
+        display: block;
     }
-    .title{
-      text-align: center;
-    };
-    button{
-      display: block;
+    a {
+        text-decoration: none;
     }
-  }
+    a:hover {
+        color: #31708e;
+    }
+    .link {
+        color: #31708e;
+        font-size: 1.25em;
+        cursor: pointer;
+    }
+    .link:hover {
+        color: #31598e;
+    }
+
+    @media (max-width: 47em) {
+        .project-card {
+            padding: 20px;
+        }
+        .project-image {
+            max-width: 100%;
+        }
+        .title {
+            text-align: center;
+        }
+        button {
+            display: block;
+        }
+    }
 }
 </style>
-
-
-
